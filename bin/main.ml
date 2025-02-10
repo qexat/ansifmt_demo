9fc1950c5b26b35a24e27b794f33ca83bfd9d79d
lexa <contact@qexat.com>
2025-02-10 09:49:50 +0100

    The v2 token tree is now the new default since Ansifmt 0.2.0 (#1)
    * Use experimental token tree v2
    
    * Make sure we are using ansifmt >=0.2

--- bin/main.ml
+++ bin/main.ml
@@ -1,5 +1,6 @@
+open Ansifmt.Prelude
 open Ansifmt_demo
 open Expr
 
 let expr = (var "x" + value 3) * var "y"
-let () = Ansifmt.print_formatted expr ~using:(module Expr)
+let () = print_formatted expr ~using:(module Expr)
